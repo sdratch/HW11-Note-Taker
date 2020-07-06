b4b8479608c8611065ba760bf11a7042c8dbb611
Sam Dratch <dratch.sam1@gmail.com>
2020-07-06 16:48:52 -0400

    added express.static

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const PORT = 3000; //|| process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/",express.static(path.join(__dirname, "public")))
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
@@ -21,16 +22,16 @@ app.get("/api/notes", (req, res) => {
   });
 });
 app.post("/api/notes", (req, res) => {
-  let newData;
-  console.log(req.body);
+  let newNote = (req.body);
+  //newNote.id = 1;
   fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
     if (err) {
       throw err;
     } else {
       data = JSON.parse(data);
-      data.push(req.body);
+      data.push(newNote);
       data = JSON.stringify(data)
-      fs.writeFile(path.join(__dirname, "db/dbtest.json"),data,"utf8",(err) => {
+      fs.writeFile(path.join(__dirname, "db/db.json"),data,"utf8",(err) => {
           if (err) {
             throw err;
           }
