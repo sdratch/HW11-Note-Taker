38e35d10627108f5218bbf8a90d9c7b44d17c30d
Sam Dratch <dratch.sam1@gmail.com>
2020-07-06 16:04:44 -0400

    added get request for db.json

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
-const PORT = 3000 //|| process.env.PORT;
+const PORT = 3000; //|| process.env.PORT;
 
-
-app.get("/notes", (req,res) =>{
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-})
-app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname, "public/index.html"));
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
+  });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
